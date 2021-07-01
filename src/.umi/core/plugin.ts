a913234ec1cbfad682c6243929af4104bcbd7527
aohanhongzhi <aohanhongzhi@qq.com>
2021-07-01 16:48:45 +0800

    并发控制

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/home/eric/.npm/_npx/8349/lib/node_modules/dumi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
